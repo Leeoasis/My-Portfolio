add184bc1ee459fb67e2cbbb17bbd547bd117fc7
Leslie <Kooliesk@gmail.com>
2023-01-30 16:20:59 +0200

    Add the mobile modal

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var modalToggler = document.getElementById("navLinks");
+    if (modalToggler.style.display === "block") {
+      modalToggler.style.display = "none";
+    } else {
+      modalToggler.style.display = "block";
+    }
+  }
