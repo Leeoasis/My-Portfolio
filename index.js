01cf046502ae2c3724c694c3a90b56c3b70dd39e
Leslie <Kooliesk@gmail.com>
2023-01-30 18:00:39 +0200

    modify the modal styling

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // Get the modal hamburger icon
 let modalOpen = document.getElementById('modalOpen');
 
-//GEt the links modal
-let linksModal = document.getElementsByClassName('modal');
+// Get the modal close icon
+let modalClose = document.getElementById('modalClose');
+
+//Get the links modal
+let linksModal = document.getElementById('modal');
 
 //Add event listener that opens the links modal
 modalOpen.addEventListener('click', modalOpener);
 
+//Add event listener that closes the links modal
+modalClose.addEventListener('click', modalCloser);
+
 //Function that opens the modal
 function modalOpener(){
     linksModal.style.display = "block";
+    modalOpen.style.display = "none";
+}
+
+//Function that closes the modal
+function modalCloser(){
+    linksModal.style.display = "none";
+    modalOpen.style.display = "block";
 }
