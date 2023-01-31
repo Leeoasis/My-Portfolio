a7f309af6538851f6dc1fa33f28a1758885cdba8
Leslie <Kooliesk@gmail.com>
2023-01-31 12:50:49 +0200

    Modify the script file

--- index.js
+++ index.js
@@ -15,21 +15,3 @@ modalOpen.addEventListener('click', modalOpener);
 //Add event listener that closes the links modal
 modalClose.addEventListener('click', modalCloser);
 
-//Function that opens the modal
-function modalOpener(){
-    linksModal.classList.remove("hidden");
-    linksModal.classList.add("overlay")
-    modalOpen.classList.add("hidden");
-    backgroundBlur.classList.add("background-blur")
-    
-}
-
-//Function that closes the modal
-function modalCloser(){
-    linksModal.classList.add("hidden");
-    linksModal.classList.remove("overlay")
-    modalOpen.classList.remove("hidden");
-
-    backgroundBlur.classList.remove("background-blur")
-
-}
